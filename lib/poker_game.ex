68fcfdba259230620ce81d5f84ae85b6a9c56106
Emilio <thecraftedgem@tuta.io>
2019-05-06 20:46:00 -0600

    initial commit

---
+++ lib/poker_game.ex
@@ -0,0 +1,18 @@
+defmodule PokerGame do
+  @moduledoc """
+  Documentation for PokerGame.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PokerGame.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/poker_game_test.exs
@@ -0,0 +1,8 @@
+defmodule PokerGameTest do
+  use ExUnit.Case
+  doctest PokerGame
+
+  test "greets the world" do
+    assert PokerGame.hello() == :world
+  end
+end
